1924a167caa9e855de15b228a4f45936f1909987
Aris Fajrianto <fajrianto.dev@gmail.com>
2023-09-26 10:06:39 +0700

    Fix: menu link in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,30 +5,39 @@ const Navbar = () => {
     <nav className="sticky top-0 z-50 bg-gradient-to-b from-black to-transparent">
       <div className="container py-10">
         <div className="flex items-center justify-between">
-          <h3 className="text-2xl font-bold uppercase text-white">
+          <Link href="/" className="text-2xl font-bold uppercase text-white">
             Movie Library
-          </h3>
+          </Link>
           <div className="flex items-center space-x-24">
             <ul className="flex space-x-16">
               <li>
-                <Link href="/" className="font-medium text-white">
+                <Link
+                  href="/"
+                  className="font-medium text-white hover:underline"
+                >
                   Movies
                 </Link>
               </li>
               <li>
-                <Link href="/" className="font-medium text-white">
+                <Link
+                  href="/"
+                  className="font-medium text-white hover:underline"
+                >
                   TV Shows
                 </Link>
               </li>
               <li>
-                <Link href="/" className="font-medium text-white">
+                <Link
+                  href="/"
+                  className="font-medium text-white hover:underline"
+                >
                   Pricing
                 </Link>
               </li>
             </ul>
             <Link
               href="/"
-              className="rounded-lg bg-yellow-600 px-6 py-2 font-medium text-white"
+              className="rounded-lg border border-yellow-600 bg-yellow-600 px-6 py-2 font-medium text-white transition-colors duration-200 hover:bg-transparent"
             >
               Sign in
             </Link>
